c4b6b1a3ed5ea1dab4d94f2398a1cdf2f6f863de
Islanilton Rodrigues <islanilton.rodrigues@gmail.com>
2020-08-03 19:12:42 -0300

    chore: add jest to lint staged

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 
 module.exports = {
-  roors: ['<rootDir/src'],
-  collectCoverageFrom: ['<rootDir/src/**/*.ts>'],
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts>'],
   coverageDirectory: 'coverage',
   coverageProvider: 'v8',
   testEnvironment: 'node',
