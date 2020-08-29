e321062b6fb958fb012e0632c3c81ccc13ae6154
Islanilton <islanilton.rodrigues@gmail.com>
2020-08-29 11:08:32 -0300

    feat: ensure cors is enable

---
+++ src/main/middlewares/cors.ts
@@ -0,0 +1,8 @@
+import { Response, Request, NextFunction } from 'express'
+
+export const cors = (req: Request, res: Response, next: NextFunction): void => {
+  res.set('access-controll-allow-origin', '*')
+  res.set('access-controll-allow-methods', '*')
+  res.set('access-controll-allow-headers', '*')
+  next()
+}
