7cc2c9b6d671ed73fa44a2fc67f10d541e2c4879
Islanilton <islanilton.rodrigues@gmail.com>
2020-08-31 19:50:52 -0300

    refactor: connect to mongodb before integrations test

--- src/main/routes/sigup-routes.test.ts
+++ src/main/routes/sigup-routes.test.ts
@@ -1,7 +1,20 @@
 import request from 'supertest'
 import app from '../config/app'
+import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 
 describe('SignUp Routes', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    const accountCollection = MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
   test('Should return an account on success', async () => {
     await request(app)
       .post('/api/signup')
@@ -9,7 +22,7 @@ describe('SignUp Routes', () => {
         name: 'islanilton',
         email: 'nito.batera@gmail.com',
         password: '123456',
-        passwordConfirmaction: '123456'
+        passwordConfirmation: '123456'
       })
       .expect(200)
   })
